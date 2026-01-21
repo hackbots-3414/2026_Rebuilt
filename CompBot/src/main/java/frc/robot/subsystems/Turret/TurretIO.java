ca2921c70fdc5cf6d176b5527a49e2c1bbcb6825
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2026-01-21 18:53:40 -0500

    Changed TurretIO

--- CompBot/src/main/java/frc/robot/subsystems/Turret/TurretIO.java
+++ CompBot/src/main/java/frc/robot/subsystems/Turret/TurretIO.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.Turret;
 
 public interface TurretIO {
   
-  public class ElevatorIOInputs {
+  public class TurretIOInputs {
     public boolean turretMotorConnected = true;
     public double turretVoltage = 0.0;
     public double turretCurrent = 0.0;
@@ -11,7 +11,12 @@ public interface TurretIO {
     public double turretPosition = 0.0;
     public double position = 0.0;
     public double reference = 0.0;
+    public double gear1CANcoder = 0;
+    public double gear2CANcoder = 0;
+    public double turretCANcoder = 0;
   }
+
+  void updateInputs(TurretIOInputs inputs);
 
   void setPosition(double position);
 
