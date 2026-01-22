9ef57d3628b09e782e8c62240ab41e59d6009c5d
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2026-01-21 20:25:03 -0500

    added shooter hardware + sim, placeholder shooter constants.

--- CompBot/src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ CompBot/src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,15 +1,28 @@
 package frc.robot.subsystems.shooter;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+
 public interface ShooterIO {
     void updateInputs (shooterIOInputs inputs);
 
     class shooterIOInputs {
         public boolean motorConnected = true;
-        public double current = 0.0;
-        public double torque = 0.0;
-        public double voltage = 0.0;
-        public double temperature = 0.0;
-        public double stator = 0.0;
+        public Current supplyCurrent = Amps.zero();
+        public Current torqueCurrent = Amps.zero();
+        public Current statorCurrent = Amps.zero();
+        public Voltage voltage = Volts.zero();
+        public Temperature temperature = Celsius.zero();
+        public AngularVelocity velocity = RotationsPerSecond.zero();
     }
-    void setVoltage(double voltage);
+
+    //void setVoltage(double voltage);
+    void setVelocity(AngularVelocity velocity);
 }
---
+++ CompBot/src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.Robot;
+
+public class ShooterIOSim implements ShooterIO {
+    private final DCMotorSim motor;
+
+    public ShooterIOSim() {
+        motor = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60Foc(1), 2, 10),
+            DCMotor.getKrakenX60Foc(1),
+            0.01,
+            0.02);
+    }
+
+    public void updateInputs(shooterIOInputs inputs) {
+        motor.update(Robot.kDefaultPeriod);
+        inputs.supplyCurrent = Amps.of(motor.getCurrentDrawAmps());
+        inputs.voltage = Volts.of(motor.getInputVoltage());
+        inputs.velocity = motor.getAngularVelocity();
+    }
+
+    public void setVelocity(AngularVelocity velocity) {
+        motor.setAngularVelocity(velocity.baseUnitMagnitude());
+    }
+}
