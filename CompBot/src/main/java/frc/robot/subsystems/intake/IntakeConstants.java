fb51e00d615914cbfeca5506c2a58275e1c3eb3d
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2026-03-07 09:10:56 -0500

    Partially added abolute encoder for intake

--- CompBot/src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ CompBot/src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -6,16 +6,21 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.FovParamsConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.ProximityParamsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.ToFParamsConfigs;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.ctre.phoenix6.signals.UpdateModeValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
@@ -29,7 +34,14 @@ public class IntakeConstants {
   protected static final Voltage kIntakeVoltage = Volts.of(5.0);
   protected static final Voltage kEjectVoltage = Volts.of(-5);
 
+  protected static final int kCANcoderId = 50; // FIXME Placeholder, replace with actual value
+
   protected static final TalonFXConfiguration kIntakeMotorConfig = new TalonFXConfiguration()
+       .withFeedback(new FeedbackConfigs()
+            .withFeedbackSensorSource(FeedbackSensorSourceValue.RemoteCANcoder)
+            .withFeedbackRemoteSensorID(kCANcoderId)
+            .withSensorToMechanismRatio(1))
+  
       .withMotorOutput(new MotorOutputConfigs()
           .withNeutralMode(NeutralModeValue.Coast)
           .withInverted(InvertedValue.Clockwise_Positive))
@@ -39,6 +51,12 @@ public class IntakeConstants {
           .withStatorCurrentLimitEnable(true)
           .withSupplyCurrentLimit(80)
           .withStatorCurrentLimit(120));
+
+    protected static final CANcoderConfiguration kCANcoderConfig = new CANcoderConfiguration()
+            .withMagnetSensor(new MagnetSensorConfigs()
+                .withSensorDirection(SensorDirectionValue.Clockwise_Positive)
+                .withAbsoluteSensorDiscontinuityPoint(1) // We'll never get to this point
+                .withMagnetOffset(0)); // FIXME Replace with actual values
 
   protected static final Current kJamStatorThreshold = Amps.of(70);
   protected static final AngularVelocity kJamVelocityThreshold = RotationsPerSecond.of(0.3);
