6732ef6c1ab212f33e9734e754770ebf70f7aa27
3414 Linux <team3414hackbots@gmail.com>
2026-03-12 18:42:12 -0400

    depot auton finished

--- CompBot/src/main/java/frc/robot/binding/AutogenBindings.java
+++ CompBot/src/main/java/frc/robot/binding/AutogenBindings.java
@@ -1,6 +1,7 @@
 package frc.robot.binding;
 
 import com.therekrab.autopilot.APTarget;
+import com.therekrab.autopilot.Autopilot;
 
 import frc.autogen.Autogen;
 import frc.robot.Constants.AutonConstants;
@@ -11,6 +12,7 @@ import frc.robot.commands.RunClimb;
 import frc.robot.commands.RunIntake;
 import frc.robot.commands.ShootWhenReady;
 import frc.robot.subsystems.climber.ClimberConstants.ClimbPosition;
+import frc.robot.subsystems.drivetrain.AutopilotConstants;
 import frc.robot.superstructure.Superstructure;
 import frc.robot.util.FieldUtils;
 
@@ -30,37 +32,44 @@ public class AutogenBindings implements Binder {
         Autogen.registerCommand("Seed Left", superstructure.build(new ResetOdometry(AutonConstants.kLeftStart, true)));
         Autogen.registerCommand("Seed Right", superstructure.build(new ResetOdometry(AutonConstants.kRightStart, true)));
 
-        registerAPTarget("Cross Right", AutonConstants.kCrossRight, superstructure);
-        registerAPTarget("Cross Right Closer", AutonConstants.kCrossRightCloser, superstructure);
-        registerAPTarget("Cross Right Angle", AutonConstants.kCrossRightAngle, superstructure);
-
-        registerAPTarget("Cross Left", AutonConstants.kCrossLeft, superstructure);
-        registerAPTarget("Cross Left Closer", AutonConstants.kCrossLeftCloser, superstructure);
-        registerAPTarget("Cross Left Angle", AutonConstants.kCrossLeftAngle, superstructure);
+        registerAPTargetLoose("Cross Right", AutonConstants.kCrossRight, superstructure);
+        registerAPTargetLoose("Cross Right Closer", AutonConstants.kCrossRightCloser, superstructure);
+        registerAPTargetLoose("Cross Right Angle", AutonConstants.kCrossRightAngle, superstructure);
         
-        registerAPTarget("Surf Left", AutonConstants.kSurfLeft, superstructure);
-        registerAPTarget("Surf Left Second", AutonConstants.kSurfLeftSecond, superstructure);
-        registerAPTarget("Surf Left Close",AutonConstants.kSurfLeftClose, superstructure);  
+        registerAPTargetLoose("Cross Left", AutonConstants.kCrossLeft, superstructure);
+        registerAPTargetLoose("Cross Left Closer", AutonConstants.kCrossLeftCloser, superstructure);
+        registerAPTargetLoose("Cross Left Angle", AutonConstants.kCrossLeftAngle, superstructure);
+        
+        registerAPTargetLoose("Surf Left", AutonConstants.kSurfLeft, superstructure);
+        registerAPTargetLoose("Surf Left Second", AutonConstants.kSurfLeftSecond, superstructure);
+        registerAPTargetLoose("Surf Left Close",AutonConstants.kSurfLeftClose, superstructure);  
 
-        registerAPTarget("Surf Right", AutonConstants.kSurfRight, superstructure);
-        registerAPTarget("Surf Right Second", AutonConstants.kSurfRightSecond, superstructure);
-        registerAPTarget("Surf Right Close",AutonConstants.kSurfRightClose, superstructure);
+        registerAPTargetLoose("Surf Right", AutonConstants.kSurfRight, superstructure);
+        registerAPTargetLoose("Surf Right Second", AutonConstants.kSurfRightSecond, superstructure);
+        registerAPTargetLoose("Surf Right Close",AutonConstants.kSurfRightClose, superstructure);
 
-        registerAPTarget("Surf Up Right", AutonConstants.kSurfUpRight, superstructure);
-        registerAPTarget("Surf Up Left", AutonConstants.kSurfUpLeft, superstructure);
+        registerAPTargetLoose("Surf Up Right", AutonConstants.kSurfUpRight, superstructure);
+        registerAPTargetLoose("Surf Up Left", AutonConstants.kSurfUpLeft, superstructure);
 
-
-        registerAPTarget("Return Left", AutonConstants.kReturnLeft, superstructure);
-        registerAPTarget("Return Left Angle", AutonConstants.kReturnLeftAngle, superstructure);        
-        registerAPTarget("Return Right", AutonConstants.kReturnRight, superstructure);
-        registerAPTarget("Return Right Angle", AutonConstants.kReturnRightAngle, superstructure);
+        registerAPTargetLoose("Return Left", AutonConstants.kReturnLeft, superstructure);
+        registerAPTargetLoose("Return Left Angle", AutonConstants.kReturnLeftAngle, superstructure);        
+        registerAPTargetLoose("Return Right", AutonConstants.kReturnRight, superstructure);
+        registerAPTargetLoose("Return Right Angle", AutonConstants.kReturnRightAngle, superstructure);
 
         registerAPTarget("Depot", AutonConstants.kDepot, superstructure);
         registerAPTarget("Tower", AutonConstants.kTower, superstructure);
         registerAPTarget("Outpost", AutonConstants.kOutpost, superstructure);
     }
 
+    private void registerAPTarget(String name, APTarget target, Superstructure superstructure, Autopilot autopilot) {
+        Autogen.registerCommand(name, superstructure.build(new DriveToPoint(() -> FieldUtils.targetFlip(target), autopilot)));
+    }
+
     private void registerAPTarget(String name, APTarget target, Superstructure superstructure) {
-        Autogen.registerCommand(name, superstructure.build(new DriveToPoint(() -> FieldUtils.targetFlip(target))));
+        registerAPTarget(name, target, superstructure, AutopilotConstants.kDefaultAutopilot);
+    }
+
+    private void registerAPTargetLoose(String name, APTarget target, Superstructure superstructure) {
+        registerAPTarget(name, target, superstructure, AutopilotConstants.kLooseAutopilot);
     }
 }
