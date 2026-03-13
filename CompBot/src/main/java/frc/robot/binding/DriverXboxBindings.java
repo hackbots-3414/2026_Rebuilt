04a5f89b24f03783f78b3bf22c0064cd2ab5b1dc
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2026-03-12 21:35:08 -0400

    remove unused intaking logic

--- CompBot/src/main/java/frc/robot/binding/DriverXboxBindings.java
+++ CompBot/src/main/java/frc/robot/binding/DriverXboxBindings.java
@@ -30,8 +30,6 @@ public class DriverXboxBindings implements Binder {
 
   private final Trigger shoot, intake, resetPerspective, robotRelativeDrive;
 
-  private boolean intaking = false;
-
   public DriverXboxBindings() {
     controller = new CommandXboxController(Driver.kDriveControllerPort);
 
@@ -48,7 +46,7 @@ public class DriverXboxBindings implements Binder {
   public void bind(Superstructure superstructure) {
     superstructure.bindDrive(vx, vy, vrot, () -> robotRelativeDrive.getAsBoolean() ? TeleopDriveMode.RobotRelative : TeleopDriveMode.FieldRelativeSpin);
 
-    shoot.toggleOnTrue(superstructure.build(new AimPrep()).alongWith(Commands.runOnce(() -> intaking = true)).finallyDo(() -> intaking = false));
+    shoot.toggleOnTrue(superstructure.build(new AimPrep()));
     intake.toggleOnTrue(superstructure.build(new RunIntake()));
     resetPerspective.onTrue(superstructure.build(new ResetForwards()));
 
