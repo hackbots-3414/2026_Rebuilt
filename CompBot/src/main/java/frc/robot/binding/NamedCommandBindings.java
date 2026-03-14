edd81a37541d416418787be22274f501e707adf5
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2026-03-14 10:56:26 -0400

    one auton/path done, sai will make the rest

--- CompBot/src/main/java/frc/robot/binding/NamedCommandBindings.java
+++ CompBot/src/main/java/frc/robot/binding/NamedCommandBindings.java
@@ -1,8 +1,14 @@
 package frc.robot.binding;
 
+import com.pathplanner.lib.auto.NamedCommands;
+
+import frc.robot.commands.AimPrep;
+import frc.robot.commands.RunIndex;
 import frc.robot.superstructure.Superstructure;
 
 public class NamedCommandBindings implements Binder {
     public void bind(Superstructure superstructure) {
+        NamedCommands.registerCommand("Aim", superstructure.build(new AimPrep()));
+        NamedCommands.registerCommand("Deploy Intake", superstructure.build(new RunIndex()));
     }    
 }
