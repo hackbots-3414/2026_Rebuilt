5deb6cf1bcd103bd67994e170d1156c7bc88f953
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2026-04-10 20:16:35 -0400

    fixed a few autons

--- CompBot/src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ CompBot/src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -617,7 +617,7 @@ public class Drivetrain extends TunerSwerveDrivetrain implements Subsystem {
         new PPHolonomicDriveController( // PPHolonomicController is the built in path following
                                         // controller for holonomic
                                         // drive trains
-            new PIDConstants(1.7, 0.0, 0.0), // Translation PID constants
+            new PIDConstants(3.0, 0.0, 0.0), // Translation PID constants
             new PIDConstants(3.0, 0.0, 0.0) // Rotation PID constants
         ),
         config, // The robot configuration
